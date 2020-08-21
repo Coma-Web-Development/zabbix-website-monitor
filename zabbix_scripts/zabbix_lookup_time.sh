7f95c3dcb81b533256de087099f50edc5cd45f1a
apterix <apterix@gmail.com>
2020-08-21 14:27:49 -0300

    add new scripts connect time and lookup time

---
+++ zabbix_scripts/zabbix_lookup_time.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+url=$1
+protocol=$2
+curl_result=$(curl -s -w  %{time_namelookup} -o /dev/null ${protocol}://${url})
+curl_return=$?
+
+bc_calc=$(echo "curl_return > 0" | bc -l)
+
+if [ $bc_calc -eq 0 ]
+then
+  echo $curl_result
+else
+  echo -1
+fi
